3dd5eea3c47e3077d214afd77d5023c5fc0aee53
Noah-FE <qinmengyao233@gmail.com>
2023-02-02 15:43:33 +0800

    feat: layout init

---
+++ next-app/components/layout/index.tsx
@@ -0,0 +1,24 @@
+import { FC } from "react";
+import JSXStyle from "styled-jsx/style";
+import { IFooterProps, Footer } from "../footer";
+import { INavBarProps, NavBar } from "../navbar";
+import styles from "./styles.module.scss";
+
+export interface ILayoutProps {
+  navbarData: INavBarProps;
+  footerData: IFooterProps;
+}
+
+export const Layout: FC<ILayoutProps & {children: JSX.Element}> = ({
+  navbarData,
+  footerData,
+  children
+}) => {
+  return (
+    <div className={styles.layout}>
+      <NavBar {...navbarData} />
+      <main className={styles.main}>{children}</main>
+      <Footer {...footerData} />
+    </div>
+  )
+}
--- next-app/pages/_app.tsx
+++ next-app/pages/_app.tsx
@@ -1,6 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Layout, ILayoutProps } from '@/components/layout'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp (data: AppProps & ILayoutProps) {
+  const { Component, pageProps, navbarData, footerData } = data;
+
+  return (
+    <Layout navbarData={navbarData} footerData={footerData} >
+      <Component {...pageProps} />
+    </Layout>
+  )
 }

e0e39c6b5c19706458ab825333a9089a5e2183e4
Noah-FE <qinmengyao233@gmail.com>
2023-02-08 22:44:50 +0800

    feat: add strapi CMS control

--- nextjs-cms/src/api/layout/controllers/layout.js
+++ nextjs-cms/src/api/layout/controllers/layout.js
@@ -1,9 +1,20 @@
-'use strict';
+// ./src/api/layout/controllers/layout.js
+"use strict";
+const { removeTime, removeAttrsAndId } = require("../../../utils/index.js");
 
 /**
- * layout controller
+ *  layout controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::layout.layout');
+module.exports = createCoreController("api::layout.layout", ({ strapi }) => ({
+  async find(ctx) {
+    ctx.query = {
+      ...ctx.query,
+      populate: "deep",
+    };
+    const { data } = await super.find(ctx);
+    return removeAttrsAndId(removeTime(data[0]));
+  },
+}));
